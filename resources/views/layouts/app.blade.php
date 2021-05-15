602064faec794f416ec4bbead72943ea6f7ea1a5
Maks Lisitsyn <maxxfoxx2012@gmail.com>
2021-05-15 23:08:49 +0300

    replace favicon
    Add favicon fittable for the website.
    Add favicon variations for different suits (apple, chrome etc.) with
    a manifest (both variations and manifest were downloaded from
    https://favicon.io/favicon-converter/ and inserted with the guidelines of
    https://evilmartians.com/chronicles/how-to-favicon-in-2021-six-files-that-fit-most-needs).

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title') — {{ config('app.name') }}</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="icon" href="{{ asset('favicon.ico') }}">
+    <link rel="icon" href="{{ asset('favicon-16x16.png') }}" type="image/png" sizes="16x16">
+    <link rel="icon" href="{{ asset('favicon-32x32.png') }}" type="image/png" sizes="32x32">
+    <link rel="apple-touch-icon" href="{{ asset('aple-touch-icon.php') }}">
+    <link rel="manifest" href="{{ asset('site.webmanifest') }}">
 </head>
 <body class="w-screen h-screen overflow-x-hidden bg-gray-200">
 @yield('body')
