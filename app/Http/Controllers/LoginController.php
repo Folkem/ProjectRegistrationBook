2fee570c01c744f16dbbdd469ffa106052d8d0ac
Folkem <maxxfoxx2012@gmail.com>
2021-05-29 16:44:57 +0300

    Change successfull auth redirecting
    Previously successfully authorized user was redirected to the projects
    page. Redirect him now to the project registration page.

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -24,7 +24,7 @@ class LoginController extends Controller
             ]);
         }
 
-        return redirect('/');
+        return redirect(route('projects.create'));
     }
 
     public function logout()
